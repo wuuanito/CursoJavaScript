e47b8e5e7d7bf31a5fcba23652e3bbd16696a331
Juan Frauca <juanfrauca@outlook.com>
2024-08-21 09:47:26 +0200

    OPERADORES LOGICOS

---
+++ Introduccion/operadoresLogicos.js
@@ -0,0 +1,20 @@
+
+//OPERADORES LOGICOS
+/*
+
+    && AND
+    || OR
+    ! NOT
+
+*/
+
+
+const nombre ='Juan';
+const edad=17;
+const tieneEntrada=true;
+const permitirAcceso = edad>=18 && tieneEntrada ;
+
+console.log('Acceso permitido : '+permitirAcceso);
+
+const falso = false;
+console.log(!falso);
