dbe6b9a72966e2b39041711fb6b2af220eae3543
Juan Frauca <juanfrauca@outlook.com>
2024-08-21 10:30:44 +0200

    templateStrgins

---
+++ Introduccion/templateStrings.js
@@ -0,0 +1,7 @@
+const nombre ='juan';
+const apellido='Frauca';
+const edad=27;
+
+console.log('La persona es '+nombre+' '+apellido);
+
+console.log(`La persona es  ${nombre} y su edad es ${edad}`);
