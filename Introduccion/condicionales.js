44bf1b5864a953cc1f27c2053c9f064199a8d7d9
Juan Frauca <juanfrauca@outlook.com>
2024-08-21 15:02:11 +0200

    Condicionales

---
+++ Introduccion/condicionales.js
@@ -0,0 +1,14 @@
+const usuario={
+    nombre:'juan',
+    tickets:true,
+    edad:17,
+}
+
+
+if (usuario.edad>17){
+
+    console.log("El usuario es mayor de edad")
+
+}else {
+    console.log ('El usuario es menor de edad ')
+}
