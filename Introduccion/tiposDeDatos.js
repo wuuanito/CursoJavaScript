5dd03eb6fd097ea31142dca133d6c28a27d39a00
Juan Frauca <juanfrauca@outlook.com>
2024-08-21 09:31:11 +0200

    STRING,NUMEROS,BOLEANOS,ARRAYS,OBJETOS,FUNCIONES

---
+++ Introduccion/tiposDeDatos.js
@@ -0,0 +1,37 @@
+//CADENA DE TEXTO
+
+const nomnbre= 'Juan'; 
+
+
+//NUMEROS
+
+const numero1=1;
+
+
+//BOLEANO
+
+const usuarioConectado=false;
+const usuarioConectado2=true;
+
+
+//ARRAYS
+
+const array = [1,2,3];
+
+const arrayCualquierTipo = ['juan',1234,[1,2,3]];
+
+//OBJETOS
+
+const persona = {
+    nombre:'Juan',
+    apellido:'Frauca',
+    edad:27,
+    coche:{
+        marca:'ford',
+        caballos:10
+    }
+}
+
+
+//FUNCIONES
+
