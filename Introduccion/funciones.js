907c32ddea9e5a1d01e0ad5c042a4f6479497e8d
Juan Frauca <juanfrauca@outlook.com>
2024-08-21 15:30:37 +0200

    Funciones

---
+++ Introduccion/funciones.js
@@ -0,0 +1,18 @@
+//FORMA 1
+
+function saludo(){
+    console.log('Saludar');
+}
+
+
+//FORMA 2
+
+const saludo1 = function (){
+    console.log('Saludar Raro');
+
+}
+
+ //FORMA 3
+
+ const saludi3 = ()=> console.log('Lamda');
+ 
