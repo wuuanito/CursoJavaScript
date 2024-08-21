541a1f7414a5f74f331c4e3f4010460374ba0dbc
Juan Frauca <juanfrauca@outlook.com>
2024-08-21 15:15:01 +0200

    Operador Ternario

---
+++ Introduccion/operadorTernario.js
@@ -0,0 +1,14 @@
+const boleto = 'vip'
+const codigoDeAcceso = (boleto === 'vip') ? 'VIP-123-456' : 'REGULAR-1234-567';
+console.log(codigoDeAcceso);
+
+/*
+ if(boleto === 'vip'){
+ 
+ codigoDeAcceso = 'VIP-123-456'
+ }else{
+    codigoDeAcceso='REGULAR-124-456'
+ }
+
+
+*/
