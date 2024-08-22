e7dfa033f26a22b4f49be32eb2cb9a67d96a3a7b
Juan Frauca <juanfrauca@outlook.com>
2024-08-22 08:56:50 +0200

    scopeBloque

--- Introduccion/scopeBloque.js
+++ Introduccion/scopeBloque.js
@@ -1,1 +1,10 @@
 //Pertene todas las variables declaradas con const o let dentro de un bloque
+
+const edad= 19;
+const accesoPermitido=false;
+if (edad>=18){
+    const accesoPermitido=true; //SOLO SE PUEDE ACCEDER DESDE EL BLOQUE
+    console.log(accesoPermitido);
+}
+
+console.log(accesoPermitido);
